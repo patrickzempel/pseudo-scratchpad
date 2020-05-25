68b3ff6e5fc8643a73c6ea7010ab2ee52baf966a
Patrick Zempel <65407354+patrickzempel@users.noreply.github.com>
2020-05-25 23:11:48 +0200

    Uploaded gif and scripts

---
+++ is_toggled.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+if [ -s /tmp/last_active_window ]; then
+	echo ""
+#       In case you don't want an Icon or haven't the NerdFonts installed
+#       you still can just output text
+#       echo "Scratch"
+else
+	echo ""
+fi
