8a59b4372abce8aa26f20ced0922b06647fc9920
Patrick Zempel <65407354+patrickzempel@users.noreply.github.com>
2020-06-12 21:32:18 +0200

    Update toggle_window.sh
    Added the option to save the toggled window geometry to move it to the same position where it was toggled

--- toggle_window.sh
+++ toggle_window.sh
@@ -2,10 +2,15 @@
 
 file=/tmp/last_active_window
 if [[ -s $file ]] ; then
-    xdotool windowmap `cat $file`
+    wid=`head -1 $file`
+    geo=`tail -1 $file`
+    xdotool windowmap $wid
+    xdotool windowmove $wid $geo
     cat /dev/null > $file
 else 
     wid=`xdotool getactivewindow`      
+    geo=`xdotool getwindowgeometry $wid | grep 'Position' | cut -f4 -d' ' | tr ',' ' '`
     xdotool windowunmap $wid
     echo $wid > $file
+    echo $geo >> $file
 fi
